a56d3dd7201e5ed8aa8c35f19d36557663fe54cc
Luiz Krüger <46863600+itisluiz@users.noreply.github.com>
2023-10-21 15:36:42 -0300

    Clone modal before Id change
    Clone modals to prevent unexpected side-effects on the modal builder object.

--- src/discord/modals.js
+++ src/discord/modals.js
@@ -1,10 +1,12 @@
+import { ModalBuilder } from 'discord.js';
 import { hashIdBuilder } from '../util/hashing.js';
 
 export async function showAndAwaitModal(interaction, modal, timeout = 60000)
 {
-	modal.setCustomId(hashIdBuilder(modal.data.custom_id));
-	await interaction.showModal(modal);
-	
-	try { return await interaction.awaitModalSubmit({filter: (modalInteraction) => modalInteraction.customId === modal.data.custom_id, time: timeout}); }
+	const uniqueModal = ModalBuilder.from(modal)
+		.setCustomId(hashIdBuilder(modal.data.custom_id));
+
+	await interaction.showModal(uniqueModal);
+	try { return await interaction.awaitModalSubmit({filter: (modalInteraction) => modalInteraction.customId === uniqueModal.data.custom_id, time: timeout}); }
 	catch { return null; };
 }
